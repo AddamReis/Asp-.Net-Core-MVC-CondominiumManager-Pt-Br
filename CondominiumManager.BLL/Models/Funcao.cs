222b92e5a3650173da0d95d7131426182987e2d8
Addam Reis <addamreis@gmail.com>
2020-08-10 20:24:11 -0300

    Configured project structure, connection DB, included class part 1...

---
+++ CondominiumManager.BLL/Models/Funcao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+
+namespace CondominiumManager.BLL.Models
+{
+    public class Funcao : IdentityRole<string>
+    {
+    }
+}
---
+++ CondominiumManager.BLL/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CondominiumManager.BLL.Models
+{
+    public class Usuario : IdentityUser<string>
+    {
+        public string CPF { get; set; }
+
+        public string FOTO { get; set; }
+
+        public bool PrimeiroAcesso { get; set; }
+
+        public StatusConta Status { get; set; }
+
+        public virtual ICollection<Apartamento> MoradorApartamentos { get; set; }
+
+        public virtual ICollection<Apartamento> ProprietarioApartamentos { get; set; }
+
+        public virtual ICollection<Veiculo> Veiculos { get; set; }
+
+        public virtual ICollection<Evento> Eventos { get; set; }
+
+        public virtual ICollection<Servico> Servicos { get; set; }
+
+        public virtual ICollection<Pagamento> Pagamentos { get; set; }
+    }
+
+    public enum StatusConta
+    {
+        Analisando = 0,
+        Aprovado = 1, 
+        Reprovado = 2
+    }
+}
