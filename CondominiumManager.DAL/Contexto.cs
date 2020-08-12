14022ff0f1721ebd5c5c1aeedafb0d86335c7864
Addam Reis <addamreis@gmail.com>
2020-08-12 20:04:18 -0300

    Add Migration

--- CondominiumManager.DAL/Contexto.cs
+++ CondominiumManager.DAL/Contexto.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using CondominiumManager.BLL.Models;
+using CondominiumManager.DAL.Mapeamentos;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Text;
 
 namespace CondominiumManager.DAL
 {
-    public class Contexto
+    public class Contexto : IdentityDbContext<Usuario, Funcao, string>
     {
+        public Microsoft.EntityFrameworkCore.DbSet<Aluguel> Alugueis { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Apartamento> Apartamentos { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Evento> Eventos { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Funcao> Funcoes { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<HistoricoRecursos> HistoricoRecursos { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Mes> Meses { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Pagamento> Pagamentos { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Servico> Servicos { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<ServicoPredio> ServicoPredios { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Usuario> Usuarios { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<Veiculo> Veiculos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new AluguelMap());
+            builder.ApplyConfiguration(new ApartamentoMap());
+            builder.ApplyConfiguration(new EventoMap());
+            builder.ApplyConfiguration(new FuncaoMap());
+            builder.ApplyConfiguration(new HistoricoRecursosMap());
+            builder.ApplyConfiguration(new MesMap());
+            builder.ApplyConfiguration(new ServicoMap());
+            builder.ApplyConfiguration(new ServicoPrediosMap());
+            builder.ApplyConfiguration(new UsuarioMap());
+            builder.ApplyConfiguration(new VeiculoMap());
+        }
     }
 }
+
