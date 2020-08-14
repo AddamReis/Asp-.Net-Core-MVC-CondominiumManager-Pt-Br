379965669c3f3820b487903afa8f9a3d15cec9ad
Addam Reis <addamreis@gmail.com>
2020-08-14 19:40:08 -0300

    User register part 2 (interface implementation)

---
+++ CondominiumManager.DAL/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,89 @@
+﻿using CondominiumManager.BLL.Models;
+using CondominiumManager.DAL.Interface;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondominiumManager.DAL.Repositorio
+{
+    public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
+    {
+        public UsuarioRepositorio(Contexto contexto) : base(contexto)
+        {
+        }
+
+        public Task AtualizarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CodificarSenha(Usuario usuario, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> CriarUsuario(Usuario usuario, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeslogarUsuario()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> IncluirUsuarioEmFuncoes(Usuario usuario, IEnumerable<string> funcoes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LogarUsuario(Usuario usuario, bool lembrar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> PegarFuncoesUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> PegarUsuarioPeloEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> PegarUsuarioPeloId(string usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> PegarUsuarioPeloNome(ClaimsPrincipal usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> RemoverFuncoesUsuario(Usuario usuario, IEnumerable<string> funcoes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int VerificarSeExisteRegistro()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> VerificarSeUsuarioEstaEmFuncao(Usuario usuario, string funcao)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
